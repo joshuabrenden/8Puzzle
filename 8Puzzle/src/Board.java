c27df7ebd8e57bd2d772639e704eea1af607f6bd
Josh Brenden <joshuabrenden@gmail.com>
2016-03-12 18:57:48 -0700

    Implemented Board and Solver API and Solver test client.

---
+++ 8Puzzle/src/Board.java
@@ -0,0 +1,45 @@
+
+public class Board {
+
+	public Board(int[][] blocks) {
+	}
+
+	public int dimension() {
+		return 0;
+	}
+
+	public int hamming() {
+		return 0;
+	}
+
+	public int manhattan(){
+		return 0;
+	}
+
+	public boolean isGoal(){
+		return true;
+	}
+
+	public Board twin(){
+		return null;
+	}
+
+	public boolean equals(Object y){
+		return true;
+	}
+
+	public Iterable<Board> neighbors(){
+		return null;
+		
+	}
+
+	public String toString(){
+		return null;
+		
+	}
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ 8Puzzle/src/Solver.java
@@ -0,0 +1,22 @@
+
+public class Solver {
+	public Solver(Board initial){
+		
+	}
+	
+	public boolean isSolvable(){
+		return true;
+	}
+	
+	public int moves(){
+		return 0;
+	}
+	
+	public Iterable<Board> solution(){
+		return null;
+	}
+	
+	public static void main(String[] args){
+		
+	}
+}
